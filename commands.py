5b10b96e1e168bd59413e29bbaafdb769b659256
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-12-30 16:17:58 +0200

    stuff

--- commands.py
+++ commands.py
@@ -1,11 +1,19 @@
 import sys
 
+import discord
+import asyncio
+
 import config
 
 bot = config.bot
 
 
-@bot.command()
+@bot.command(
+    aliases=["latency"],
+    hidden=True,
+    help="Used to get the ping of the bot. "
+         "(Only works when called by bot mods.)"
+)
 async def ping(ctx):
     latency = round(bot.latency, 3) * 1000  # in ms to 3 d.p.
 
@@ -13,7 +21,10 @@ async def ping(ctx):
 
 
 # closes the bot (only bot owners)
-@bot.command(hidden=True)
+@bot.command(
+    hidden=True,
+    help="Used to terminate the bot. (Only works when called by bot admins.)"
+)
 async def cease(ctx):
     if not await bot.is_owner(ctx.author):
         return
@@ -23,3 +34,187 @@ async def cease(ctx):
 
     await bot.close()
     sys.exit()
+
+
+def help_pages(mod):
+    commands_list = []
+
+    for command in bot.commands:
+        if not mod:
+            if not command.hidden:
+                commands_list.append(command)
+        else:
+            if command.hidden:
+                commands_list.append(command)
+
+    commands_list.sort(key=lambda command_in: command_in.name)
+
+    grouped_commands_list = [
+        commands_list[i:i + 10] for i in range(0, len(commands_list), 10)
+    ]
+
+    pages = []
+
+    i = 0
+    total_pages = len(grouped_commands_list)
+
+    for group in grouped_commands_list:
+        page = discord.Embed(
+            title=f"Commands",
+            description=f"*Showing page {i + 1} of {total_pages}, "
+                        f"use reactions to switch pages.*",
+            color=0x9ab8d6
+        )
+
+        for command in group:
+            page.add_field(
+                name=command.name,
+                value=(
+                        command.help +
+                        (
+                            f"\n*Usage:* `{command.usage}`" if command.usage
+                            else ""
+                        ) +
+                        (
+                            f"\n*Alias"
+                            f"{'' if len(command.aliases) == 1 else 'es'}"
+                            f":* `{'`, `'.join(command.aliases)}`"
+                            if command.aliases
+                            else ""
+                        )
+                ),
+                inline=False
+            )
+
+        pages.append(page)
+
+        i += 1
+
+    return pages
+
+
+bot.remove_command("help")
+
+
+@bot.command(
+    name="help",
+    aliases=["h"],
+    help="Used for getting this message."
+)
+async def help_(ctx):
+    pages = help_pages(False)
+    total_pages = len(pages)
+
+    n = 0
+
+    help_message = await ctx.send(embed=pages[n])
+
+    await help_message.add_reaction("◀️")
+    await help_message.add_reaction("▶️")
+
+    def check(reaction_in, user_in):
+        return user_in == ctx.author and str(reaction_in.emoji) in ("◀️", "▶️")
+
+    while True:
+        try:
+            reaction, user = await bot.wait_for(
+                "reaction_add",
+                check=check,
+                timeout=90
+            )
+
+            if str(reaction.emoji) == "▶️":
+                if n + 2 > total_pages:
+                    pass
+                else:
+                    n += 1
+
+                    await help_message.edit(embed=pages[n])
+
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+            elif str(reaction.emoji) == "◀️":
+                if n == 0:
+                    pass
+                else:
+                    n -= 1
+
+                    await help_message.edit(embed=pages[n])
+
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+            else:
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+        except asyncio.TimeoutError:
+            break
+
+
+@bot.command(
+    name="mhelp",
+    aliases=["mh"],
+    help="Used for getting this message.",
+    hidden=True
+)
+async def mod_help(ctx):
+    if not bot.is_owner(ctx.author):
+        return
+
+    pages = help_pages(True)
+    total_pages = len(pages)
+
+    n = 0
+
+    help_message = await ctx.send(embed=pages[n])
+
+    await help_message.add_reaction("◀️")
+    await help_message.add_reaction("▶️")
+
+    def check(reaction_in, user_in):
+        return user_in == ctx.author and str(reaction_in.emoji) in ("◀️", "▶️")
+
+    while True:
+        try:
+            reaction, user = await bot.wait_for(
+                "reaction_add",
+                check=check,
+                timeout=90
+            )
+
+            if str(reaction.emoji) == "▶️":
+                if n + 2 > total_pages:
+                    pass
+                else:
+                    n += 1
+
+                    await help_message.edit(embed=pages[n])
+
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+            elif str(reaction.emoji) == "◀️":
+                if n == 0:
+                    pass
+                else:
+                    n -= 1
+
+                    await help_message.edit(embed=pages[n])
+
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+            else:
+                try:
+                    await help_message.remove_reaction(reaction, user)
+                except discord.Forbidden:
+                    pass
+        except asyncio.TimeoutError:
+            break
--- events.py
+++ events.py
@@ -1,10 +1,18 @@
 import config
 
+import discord
+
 bot = config.bot
 
 
 @bot.event
 async def on_ready():
     latency = round(bot.latency, 3) * 1000  # in ms to 3 d.p.
+
+    for guild in bot.guilds:
+        try:
+            await guild.me.edit(nick=f"{bot.user.name} | {bot.command_prefix}")
+        except discord.Forbidden:
+            pass
 
     print(f"Connected successfully as {bot.user} ({latency}ms).")
